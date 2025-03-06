86fe1da9b16c31f92bdd5ce1a5478f19b3b62e84
d4mp3 <damian.pszczola@gmail.com>
2025-03-06 15:25:37 +0100

    add urlp.py to blog app

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for blog application.
+"""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('/', views.blog_page, name="blog-page"),
+    path('posts/', views.posts, name="post-page"),
+    path('posts/<slug:slug>', views.post_detail, name="post-detail-page"),  #/post/my-first-post
+]
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def blog_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
+
