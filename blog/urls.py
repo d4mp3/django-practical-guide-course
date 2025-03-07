3bf4657e5bfe88d3368c02963c792068913661e6
d4mp3 <damian.pszczola@gmail.com>
2025-03-07 14:45:19 +0100

    create all-post page

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_page, name='blog-page'),
-    path('posts', views.posts, name='post-page'),
+    path('posts/', views.posts, name='posts-page'),
     path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),  #/post/my-first-post
 ]
