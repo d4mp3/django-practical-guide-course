b01b90789b8519b68e837e53c345e8be22630b02
d4mp3 <damian.pszczola@gmail.com>
2025-03-11 22:57:59 +0100

    create PostAdmin

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,20 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return " ".join([self.first_name, self.last_name])
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
@@ -18,5 +27,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(100)])
-    author_field = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
