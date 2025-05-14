754d927c0d3855b402670409a9c07d9570abd0af
d4mp3 <damian.pszczola@gmail.com>
2025-05-14 22:29:15 +0200

    output comments to post pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Post, Tag
+from .models import Author, Comment, Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -17,6 +17,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
